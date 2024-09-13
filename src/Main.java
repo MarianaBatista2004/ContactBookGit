6784999aaee476419e0851ec626a6b52e480b000
Goncalo <gc.farias@campus.fct.unl.pt>
2024-09-13 10:59:58 +0100

    Added new Main commands

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GIVEN_NUMBER   = "GN";
+    public static final String REP_PHONE      = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,9 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String NUMBER_NOT_EXIST = "Phone number does not exist.";
+    public static final String NOT_ALL_DIFFERENT = "There are contacts that share phone numbers.";
+    public static final String ALL_DIFFERENT = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +57,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GIVEN_NUMBER:
+                    getByNumber(in, cBook);
+                    break;
+                case REP_PHONE:
+                    repeatedPhones(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +157,22 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void getByNumber(Scanner in, ContactBook cBook) {
+        int phone;
+        phone = in.nextInt(); in.nextLine();
+        String name = cBook.findContactByNumber(phone);
+        if(name != null) {
+            System.out.println(name + "" + phone);
+        }
+        else System.out.println(NUMBER_NOT_EXIST);
+    }
+    
+    private static void repeatedPhones(ContactBook cBook) {
+        if(cBook.repeatedContact()) {
+            System.out.println(NOT_ALL_DIFFERENT);
+        }
+        else System.out.println(ALL_DIFFERENT);
     }
 }
